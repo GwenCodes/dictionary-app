46b93e52d16eac591b13c7d5fd6056a67aab0201
GwenCodes <essexdesigns43.0@gmail.com>
2025-11-30 17:13:22 -0500

    Started file, added logo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.png";
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-logo img-fluid">
+        <img src={logo} alt="app-logo" />
+      </header>
+    </div>
+  );
+}
